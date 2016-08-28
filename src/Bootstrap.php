2e6e47b309194a72cb5bb1931ca164f165260467
vagrant <vagrant@homestead>
2016-08-28 17:10:33 +0000

    Added composer.json. Added error handler using filp/whoops.

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,3 +1,24 @@
 <?php
 
-echo 'Hello World';
+namespace App;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+/**
+ * Registering Error Handler
+ */
+$whoops = new \Whoops\Run;
+if ($environment !== 'production') {
+  $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+  $whoops->pushHandler(function($e){
+    echo 'Something bad happened! Send an email to the developer';
+  });
+}
+$whoops->register();
+
+throw new \Exception;
